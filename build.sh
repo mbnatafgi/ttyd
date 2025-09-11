eba100a76f6c4eeecb1a7329b9201cd6f33262ae
Belal Natafgi <belal.natafgi@icloud.com>
2025-09-11 09:08:31 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-cd ../html && npm run build && cd ../build && cmake .. && make -j$(nproc)
+cd html && npm run build && cd ../build && cmake .. && make -j$(nproc) && cd ../
 launchctl kill SIGTERM gui/$(id -u)/com.user.ttyd
